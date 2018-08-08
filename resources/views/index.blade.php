bf6982deb7f4a811202332d076992bb6a1cefad0
Alvaro <alvaroqv@gmail.com>
2018-08-08 14:42:16 -0300

    Add documantation

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,21 +12,17 @@ Widgets Assignment!!!
 <li>Shows how to upload a file into the server using Laravel</li> 
 <li>Process this file using background queued process</li> 
 <li>The process should be able to stop and then continue where it stopped.</li>
+<li>Export files into Excel table</li>
 </ul>
 Link to the Upload Form: <a href="upload"> click here</a>
 <br>
 Link to list of uploaded Files: <a href="json"> click here</a>
-<br><br>
-<p>
-<b>Explaining the project:</b>
 <br>
-- Two model files - Project and ProjectFiles<br>
-The Project model: has two properties - id (Long), name (String) <br>
-The ProjectFiles model: has three properties - id (Long), project_id (Long - Project relationship) , filename (String)  <br>
-</p>
-docker exec -it testeapp_web_1 php artisan queue:table<br><br>
-docker exec -it testeapp_web_1 php artisan migrate<br><br>
-docker exec -it testeapp_web_1 php artisan queue:work --daemon<br><br>
-create php.ini file upload size
-</p>
+Link to list of processing Files table: <a href="json/processing"> click here</a>
+<br>
+Link to list of processed Files table: <a href="json/processed"> click here</a>
+<br>
+
+
+
 @stop
